bc7f7413162103bf1d65dc1bae4a48a45d5ef007
louise-ulrika <ulrika.nicander@gmail.com>
2025-11-20 16:59:01 +0100

    prompt fungerar

---
+++ script/index.js
@@ -0,0 +1,12 @@
+const anyNumber=Math.floor(Math.random()*10);
+let guess=null;
+while(guess!==anyNumber){
+    guess=prompt("Guess a number between 0 and 10:");
+    caseNumber=parseInt(guess);
+    if(guess>anyNumber){
+        console.log("Good guess!Maybe try a lower number?");
+    }else if(guess<anyNumber){
+        console.log("Good guess!Maybe try a higher number?");
+    }
+console.log('anyNumber:${anyNumber}.Guess:${guess}');
+}
