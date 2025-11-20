ced18ce96d535a630836d35c3ea425b2a1849464
louise-ulrika <ulrika.nicander@gmail.com>
2025-11-20 17:38:35 +0100

    ändringar 17.38

--- script/index.js
+++ script/index.js
@@ -2,11 +2,15 @@ const anyNumber=Math.floor(Math.random()*10);
 let guess=null;
 while(guess!==anyNumber){
     guess=prompt("Guess a number between 0 and 10:");
-    caseNumber=parseInt(guess);
-    if(guess>anyNumber){
-        console.log("Good guess!Maybe try a lower number?");
-    }else if(guess<anyNumber){
-        console.log("Good guess!Maybe try a higher number?");
+    guess=parseInt(guess)};
+    if(guess>anyNumber) {
+           return
+        console.log(prompt"Good guess!Maybe try a lower number?");}
+        
+    else if (guess<anyNumber){
+        return
+        console.log(prompt"Good guess!Maybe try a higher number?");
+        
     }
-console.log('anyNumber:${anyNumber}.Guess:${guess}');
-}
+
+console.log("Congratulations!You guessed the correct number:",anyNumber);
