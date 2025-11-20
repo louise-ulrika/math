cc8ab25871d3f7673ed7df152ae27b3f39a08d65
louise-ulrika <ulrika.nicander@gmail.com>
2025-11-20 19:09:18 +0100

    ändring med alert
    alertfunktionen fungerar

--- script/index.js
+++ script/index.js
@@ -1,13 +1,13 @@
 const anyNumber=Math.floor(Math.random()*10);
 let guess=null;
 while(guess!==anyNumber){
-    guess=parseFloat(prompt("Guess a number between 0 and 10:"));
+    guess=parseFloat(prompt("Guess a number between 1 and 10:"));
     
 
     if(guess<anyNumber){
-        console.log("Too low! Try again.");
+        alert("Too low! Try again.");
     }else if(guess>anyNumber){
-        console.log("Too high! Try again.");
+        alert("Too high! Try again.");
     }
        
  
