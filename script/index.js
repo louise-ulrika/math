4a0206e040a0a7ca1ad8f308d08e8dc2a76fe8c2
louise-ulrika <ulrika.nicander@gmail.com>
2025-11-27 15:39:28 +0100

    changes 27/11
    changes 27/11

--- script/index.js
+++ script/index.js
@@ -1,15 +1,42 @@
+
 const anyNumber=Math.floor(Math.random()*10);
-let guess=null;
-while(guess!==anyNumber){
-    guess=parseFloat(prompt("Guess a number between 1 and 10:"));
+let guess=null; 
+
+function guessNumbergame(){
     
+
+
+
+
+
+while(guess!==anyNumber){
+    guess=parseFloat(prompt("Guess a number between 1 and 10:"));}
+}
+ 
 
     if(guess<anyNumber){
         alert("Too low! Try again.");
     }else if(guess>anyNumber){
         alert("Too high! Try again.");
-    }
-       
- 
+
+    
+return guessNumbergame();
+
+
 }
-console.log("Congratulations!You guessed the correct number:",anyNumber);
+        console.log ("Congratulations! You guessed the correct number:");
+alert ("Congratulations! You guessed the correct number:" + anyNumber);
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
